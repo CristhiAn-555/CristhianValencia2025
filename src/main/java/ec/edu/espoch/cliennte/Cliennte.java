bee5326cda4f5ceacdb28c4eabede4c1956d2ba3
CristhiAn-555 <cristhianvalencia999@gmail.com>
2024-12-05 14:39:16 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/cliennte/Cliennte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.cliennte;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Cliennte {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/ec/edu/espoch/cliennte/Productos/Productos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.cliennte.Productos;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Productos {
+    
+    public String Cocacola;
+    public String agua;
+   
+    
+    
+            
+    
+}
